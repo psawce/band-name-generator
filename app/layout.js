ce309605628cf59ef41ad9fd0b396ded282dc4d8
psawce <scannarelli@gmail.com>
2026-04-23 12:36:01 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
+import "./globals.css";
+
 export const metadata = { title: "Band Name Generator" };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif", background: "#fff", color: "#111" }}>
-        {children}
-      </body>
+    <html lang="en" data-theme="night">
+      <body>{children}</body>
     </html>
   );
 }
