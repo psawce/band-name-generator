fa6422f75ba85ebae9fe7954e47808208a8df5ff
psawce <scannarelli@gmail.com>
2026-04-23 12:57:25 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="night">
       <head>
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.css" rel="stylesheet" type="text/css" />
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/base.css" rel="stylesheet" type="text/css" />
       </head>
       <body>{children}</body>
     </html>
