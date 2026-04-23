2a054bb624cb79f7e89dab857d25567baa5c9288
psawce <scannarelli@gmail.com>
2026-04-23 18:44:45 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ export const metadata = { title: "Band Name Generator" };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="retro">
+    <html lang="en" data-theme="black">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.css" rel="stylesheet" type="text/css" />
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
