e24593e5f33f358baa474b82d785beff2ded7eaa
psawce <scannarelli@gmail.com>
2026-04-24 13:20:22 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       <link rel="stylesheet" href="https://use.typekit.net/zqj2efl.css">
+        <link rel="stylesheet" href="https://use.typekit.net/zgj2efl.css" />
       </head>
       <body>{children}</body>
     </html>
