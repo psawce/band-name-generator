5dc5fac08d13fa542babcb6c5b2acd7c0ab0509f
psawce <scannarelli@gmail.com>
2026-05-01 22:09:05 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["night"],
-  },
-};
+
